e6f360046e6b6032417a354b7b2ba742925c8bf3
theothecoder <theothecoder@gmail.com>
2025-06-16 11:20:07 +0200

    implemented add to basket feature

---
+++ app/(store)/store.ts
@@ -0,0 +1,81 @@
+import { Product } from "@/sanity.types";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export interface BasketItem {
+  product: Product;
+  quantity: number;
+}
+
+interface BasketState {
+  items: BasketItem[];
+  addItem: (product: Product) => void;
+  removeItem: (productId: string) => void;
+  clearBasket: () => void;
+  getTotalPrice: () => number;
+  getItemCount: (productId: string) => number;
+  getGroupedItems: () => BasketItem[];
+}
+
+export const UseBasketStore = create<BasketState>()(
+  persist(
+    (set, get) => ({
+      items: [],
+
+      addItem: (product) =>
+        set((state) => {
+          const existingItem = state.items.find(
+            (item) => item.product._id === product._id
+          );
+
+          if (existingItem) {
+            return {
+              items: state.items.map((item) =>
+                item.product._id === product._id
+                  ? { ...item, quantity: item.quantity + 1 }
+                  : item
+              ),
+            };
+          } else {
+            return { items: [...state.items, { product, quantity: 1 }] };
+          }
+        }),
+
+      removeItem: (productId) =>
+        set((state) => ({
+          items: state.items.reduce((acc: BasketItem[], item) => {
+            if (item.product._id === productId) {
+              if (item.quantity > 1) {
+                acc.push({ ...item, quantity: item.quantity - 1 });
+              }
+              // else skip item (remove it entirely)
+            } else {
+              acc.push(item);
+            }
+            return acc;
+          }, []),
+        })),
+
+      clearBasket: () => set({ items: [] }),
+
+      getTotalPrice: () =>
+        get().items.reduce(
+          (total, item) => total + (item.product.price ?? 0) * item.quantity,
+          0
+        ),
+
+      getItemCount: (productId) => {
+        const item = get().items.find(
+          (item) => item.product._id === productId
+        );
+        return item?.quantity ?? 0;
+      },
+
+      getGroupedItems: () => get().items,
+    }),
+    {
+      name: "basket-store",
+    }
+  )
+);
+export default UseBasketStore;
