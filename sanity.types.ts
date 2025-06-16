e1b0f0b7c81654426ba06c24184afa951a9b87f6
theothecoder <theothecoder@gmail.com>
2025-06-16 09:51:21 +0200

    implemented loaders

--- sanity.types.ts
+++ sanity.types.ts
@@ -351,6 +351,71 @@ export type ALL_PRODUCTS_QUERYResult = Array<{
   stock?: number;
 }>;
 
+// Source: ./sanity/lib/products/getProductByCategory.ts
+// Variable: PRODUCT_BY_CATEGORY_QUERY
+// Query: *[_type == "product" && references(*[_type == "category" && slug.current == $categorySlug]._id)]     | order(name asc)
+export type PRODUCT_BY_CATEGORY_QUERYResult = Array<{
+  _id: string;
+  _type: "product";
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  name?: string;
+  slug?: Slug;
+  image?: {
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    media?: unknown;
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    _type: "image";
+  };
+  description?: Array<{
+    children?: Array<{
+      marks?: Array<string>;
+      text?: string;
+      _type: "span";
+      _key: string;
+    }>;
+    style?: "blockquote" | "h1" | "h2" | "h3" | "h4" | "normal";
+    listItem?: "bullet";
+    markDefs?: Array<{
+      href?: string;
+      _type: "link";
+      _key: string;
+    }>;
+    level?: number;
+    _type: "block";
+    _key: string;
+  } | {
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    media?: unknown;
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    alt?: string;
+    _type: "image";
+    _key: string;
+  }>;
+  price?: number;
+  categories?: Array<{
+    _ref: string;
+    _type: "reference";
+    _weak?: boolean;
+    _key: string;
+    [internalGroqTypeReferenceTo]?: "category";
+  }>;
+  stock?: number;
+}>;
+
 // Source: ./sanity/lib/products/getProductBySlug.ts
 // Variable: PRODUCT_BY_ID_QUERY
 // Query: *[_type =="product"        && slug.current == $slug]        |order(name asc)[0]
@@ -505,6 +570,7 @@ declare module "@sanity/client" {
   interface SanityQueries {
     "*[_type == \"category\"]  | order(name asc)": ALL_CATEGORIES_QUERYResult;
     "*[_type == \"product\"] \n      | order(name asc)": ALL_PRODUCTS_QUERYResult;
+    "\n        *[_type == \"product\" && references(*[_type == \"category\" && slug.current == $categorySlug]._id)] \n    | order(name asc)": PRODUCT_BY_CATEGORY_QUERYResult;
     "\n        *[_type ==\"product\"\n        && slug.current == $slug]\n        |order(name asc)[0]": PRODUCT_BY_ID_QUERYResult;
     "\n        *[_type ==\"product\"\n        && name match $searchParam]\n        |order(name asc)": PRODUCT_SEARCH_QUERYResult;
     "\n        *[_type == \"sales\" && isActive == true\n        && couponCode ==$couponCode\n        ]|order(validFrom desc)[0]": ACTIVE_SALE_BY_COUPON_QUERYResult;
